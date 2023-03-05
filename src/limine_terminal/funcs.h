7cabadb22ac2a6aa268d84634b2a2b280ff13d69
ilobilo <ilia.chachanidze35@gmail.com>
2023-03-05 19:29:01 +0400

    Fixes and updates

--- src/limine_terminal/funcs.h
+++ src/limine_terminal/funcs.h
@@ -2,10 +2,18 @@
 
 #include <stddef.h>
 
-extern void *term_alloc(size_t size);
-extern void *term_realloc(void *oldptr, size_t size);
-extern void term_free(void *ptr, size_t size);
-extern void term_freensz(void *ptr);
+#ifdef __cplusplus
+#  define _TERM_EXTERN extern "C"
+#else
+#  define _TERM_EXTERN extern
+#endif
 
-extern void *term_memcpy(void *dest, const void *src, size_t size);
-extern void *term_memset(void *dest, int val, size_t count);
+_TERM_EXTERN void *term_alloc(size_t size);
+_TERM_EXTERN void *term_realloc(void *oldptr, size_t size);
+_TERM_EXTERN void term_free(void *ptr, size_t size);
+_TERM_EXTERN void term_freensz(void *ptr);
+
+_TERM_EXTERN void *term_memcpy(void *dest, const void *src, size_t size);
+_TERM_EXTERN void *term_memset(void *dest, int val, size_t count);
+
+#undef _TERM_EXTERN
